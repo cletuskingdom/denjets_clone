7910d2f634b0f0279e66762c7e8fb1922bc01ed7
Cletus Kingdom <cletuskingdom@gmail.com>
2023-02-10 10:50:25 +0100

    Users can capture username and age

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const logo_1 =
+		"https://denjets.netlify.app/static/media/denjets.6ef74d44.png";
+
+	const [state, setState] = useState({ username: "", age: null });
+	const handleLogin = (event) => {
+		event.preventDefault();
+		const age = state.age;
+		const username = state.username;
+
+		if (age === "" && username === "") {
+			alert("You have not filled some field");
+			return;
+		} else if (!Number(age)) {
+			alert("Your age must be a number");
+			return;
+		} else {
+			alert("Your age: " + age + ", and your age is: " + username);
+			// console.log("submitting", state);
+		}
+	};
+
+	const handleChange = (event) => {
+		const name = event.target.name;
+		const value = event.target.value;
+		setState({ ...state, [name]: value });
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo_1} className="logo" alt="logo" />
+			</header>
+
+			<section>
+				<div className="container py-5">
+					<form onSubmit={handleLogin}>
+						<div className="mb-3">
+							<label>
+								Email:
+								<input
+									type="email"
+									className="form-control"
+									placeholder="michealf@gmail.com"
+									name="username"
+									onChange={handleChange}
+								/>
+							</label>
+						</div>
+
+						<div className="mb-3">
+							<label>
+								Age:
+								<input
+									type="text"
+									className="form-control"
+									placeholder="25"
+									name="age"
+									onChange={handleChange}
+								/>
+							</label>
+						</div>
+
+						<button
+							className="btn btn-dark px-5 btn-sm"
+							type="submit"
+						>
+							LOGIN
+						</button>
+					</form>
+				</div>
+			</section>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/pages/login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+	return (
+		<div>
+			<h5>This is the LOGIN PAGE</h5>
+		</div>
+	);
+};
+
+export default Login;
